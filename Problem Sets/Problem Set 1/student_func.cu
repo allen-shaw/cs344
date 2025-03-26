268d0ce12b75c21df498f12ddf78da20afff960d
allen <allenshaw19@gmail.com>
2025-03-26 19:28:58 +0800

    fix for cuda12 and cmake3.31

--- Problem Sets/Problem Set 1/student_func.cu
+++ Problem Sets/Problem Set 1/student_func.cu
@@ -33,12 +33,13 @@
 
 #include "utils.h"
 
+const size_t blockWidth = 32;
+
 __global__
 void rgba_to_greyscale(const uchar4* const rgbaImage,
                        unsigned char* const greyImage,
                        int numRows, int numCols)
 {
-  //TODO
   //Fill in the kernel to convert from color to greyscale
   //the mapping from components of a uchar4 to RGBA is:
   // .x -> R ; .y -> G ; .z -> B ; .w -> A
@@ -50,6 +51,17 @@ void rgba_to_greyscale(const uchar4* const rgbaImage,
   //First create a mapping from the 2D block and grid locations
   //to an absolute 2D location in the image, then use that to
   //calculate a 1D offset
+  size_t idx_x = threadIdx.x + blockIdx.x * blockDim.x;
+  size_t idx_y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (idx_x >= numCols || idx_y >= numRows) {
+    return;
+  }
+  size_t idx = idx_x * numCols + idx_y;
+  uchar4 rgbValue = rgbaImage[idx];
+
+  float greyValue = .299f * rgbValue.x + .587f * rgbValue.y + .114f * rgbValue.z;
+  greyImage[idx] = (unsigned char)greyValue;
 }
 
 void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_rgbaImage,
@@ -57,8 +69,10 @@ void your_rgba_to_greyscale(const uchar4 * const h_rgbaImage, uchar4 * const d_r
 {
   //You must fill in the correct sizes for the blockSize and gridSize
   //currently only one block with one thread is being launched
-  const dim3 blockSize(1, 1, 1);  //TODO
-  const dim3 gridSize( 1, 1, 1);  //TODO
+  const dim3 blockSize(blockWidth, blockWidth, 1);  //
+  unsigned int numBlockX = (unsigned int)(numCols / blockSize.x + 1);
+  unsigned int numBlockY = (unsigned int)(numRows / blockSize.y + 1);
+  const dim3 gridSize(numBlockX, numBlockY, 1);  
   rgba_to_greyscale<<<gridSize, blockSize>>>(d_rgbaImage, d_greyImage, numRows, numCols);
   
   cudaDeviceSynchronize(); checkCudaErrors(cudaGetLastError());
